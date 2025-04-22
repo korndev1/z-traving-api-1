ac0030d6be20383a1e1be1b8a1b93d3133a7cb84
korndev1 <tinnakorn@onelifegroupholding.com>
2025-04-22 11:15:56 +0700

    decoator guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
 
 @Controller('users')
 export class UserController {
-    // constructor(private userService: UserService){}
-    @UseGuards(AuthGuard('jwt'))
-    @Get('me')
-    getMe(){
-        return 'user info'
-    }
+  // constructor(private userService: UserService){}
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+
+  @Patch()
+  edituser(){
+    
+  }
 }
