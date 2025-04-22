34dda949b0870b35f7e52298c691a8c23485ea3f
korndev1 <tinnakorn@onelifegroupholding.com>
2025-04-22 16:30:41 +0700

    add test function

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -13,4 +13,8 @@ export class PrismaService extends PrismaClient {
       },
     });
   }
+
+  cleanDb(){
+    this.user.deleteMany()
+  }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,6 @@
 import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
-import { UserService } from './user.service';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
-import { JwtGuard } from 'src/auth/guard';
-import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from '../auth/guard';
+import { GetUser } from '../auth/decorator';
 import { User } from '@prisma/client';
 
 @Controller('users')
