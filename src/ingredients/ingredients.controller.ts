fc51567a78d674ab6380fd676e0db87451329dfb
korndev1 <tinnakorn@onelifegroupholding.com>
2025-05-06 09:58:35 +0700

    create API get delete put measure

--- src/ingredients/ingredients.controller.ts
+++ src/ingredients/ingredients.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { IngredientsService } from './ingredients.service';
 import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { MeasureDto } from './dto';
@@ -6,18 +16,32 @@ import { MeasureDto } from './dto';
 @ApiTags('Ingredients')
 @Controller('ingredients')
 export class IngredientsController {
-    constructor(private ingredientService: IngredientsService){}
+  constructor(private ingredientService: IngredientsService) {}
 
-    @HttpCode(HttpStatus.OK)
-    @Post('add-measure')
-    @ApiBody({type:MeasureDto})
-    addMeasure(@Body() dto:MeasureDto){
-        return this.ingredientService.addMeasure(dto)
-    }
+  @HttpCode(HttpStatus.OK)
+  @Post('add-measure')
+  @ApiBody({ type: MeasureDto })
+  addMeasure(@Body() dto: MeasureDto) {
+    return this.ingredientService.addMeasure(dto);
+  }
 
-    @HttpCode(HttpStatus.OK)
-    @Get('measure')
-    getMeasure(){
-        return this.ingredientService.getMeasure()
-    }
+  @HttpCode(HttpStatus.OK)
+  @Get('measure')
+  getMeasure() {
+    return this.ingredientService.getMeasure();
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Delete('/delMeasure/:id')
+  async deleteMeasure(@Param('id') id: string) {
+    const numericId = parseInt(id, 10);
+    return this.ingredientService.deleteMeasure(numericId);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Put('/updateMeasure/:id')
+  async updateMeasure(@Param('id') id: string, @Body() dto:MeasureDto) {
+    const numericId = parseInt(id, 10);
+    return this.ingredientService.updateeMeasure(numericId,dto);
+  }
 }
--- src/ingredients/ingredients.service.ts
+++ src/ingredients/ingredients.service.ts
@@ -4,32 +4,61 @@ import { MeasureDto } from './dto';
 
 @Injectable()
 export class IngredientsService {
-    constructor(private prisma: PrismaService){}
+  constructor(private prisma: PrismaService) {}
 
-    async addMeasure(dto: MeasureDto) {
-        try {
-            const measure = await this.prisma.measure.create({
-                data:{
-                    name:dto.name,
-                    short_forn:dto.short_form
-                }
-            })
+  async addMeasure(dto: MeasureDto) {
+    try {
+      const measure = await this.prisma.measure.create({
+        data: {
+          name: dto.name,
+          short_forn: dto.short_form,
+        },
+      });
 
-            return {statusCode:200,data: measure}
-        } catch (error) {
-            return {statusCode:400,data: 'error'}
-        }
-        
+      return { statusCode: 200, data: measure };
+    } catch (error) {
+      return { statusCode: 400, data: 'error' };
     }
+  }
 
-    async getMeasure(){
-        try {
-            const measure = await this.prisma.measure.findMany()
-            return{statusCode:200,data: measure}
-        } catch (error) {
-            return error
-        }
+  async getMeasure() {
+    try {
+      const measure = await this.prisma.measure.findMany();
+      return { statusCode: 200, data: measure };
+    } catch (error) {
+      return error;
     }
+  }
+
+  async deleteMeasure(id: number) {
+    try {
+      const measure = await this.prisma.measure.delete({
+        where: {
+          id: id,
+        },
+      });
+      return { statusCode: 200, data: measure };
+
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async updateeMeasure(id: number,dto:MeasureDto) {
+    try {
+      const measure = await this.prisma.measure.update({
+        where: {
+          id: id,
+        },
+        data:{
+          name:dto.name,
+          short_forn: dto.short_form
+        }
+      });
+      return { statusCode: 200, data: measure };
+
+    } catch (error) {
+      return error;
+    }
+  }
 }
-
-
